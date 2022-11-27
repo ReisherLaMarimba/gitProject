843e25dfc57b7eea559b81c738884b3d4b5d6091
ReisherLaMarimba <Reisher62@gmail.com>
2022-11-26 22:03:30 -0500

    Nuevos mensajes de error

---
+++ resources/views/Errors/404.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <header class="top-header">
+</header>
+
+<!--dust particel-->
+<div>
+  <div class="starsec"></div>
+  <div class="starthird"></div>
+  <div class="starfourth"></div>
+  <div class="starfifth"></div>
+</div>
+<!--Dust particle end--->
+
+
+<div class="lamp__wrap">
+  <div class="lamp">
+    <div class="cable"></div>
+    <div class="cover"></div>
+    <div class="in-cover">
+      <div class="bulb"></div>
+    </div>
+    <div class="light"></div>
+  </div>
+</div>
+<!-- END Lamp -->
+<section class="error">
+  <!-- Content -->
+  <div class="error__content">
+    <div class="error__message message">
+      <h1 class="message__title">Pagina inhabilitada o no Existe</h1>
+      <p class="message__text">Habla con tu admin</p>
+    </div>
+    <div class="error__nav e-nav">
+      
+    </div>
+  </div>
+  <!-- END Content -->
+
+</section>
+
+  </a>
+  @endsection
